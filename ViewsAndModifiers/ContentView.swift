ffd9d571a9ebad56028728bf23f0901c8a38afa2
jerdrb <jerdrb@gmail.com>
2022-06-27 21:03:59 -0500

    Initial Commit
    Showing quick lesson of custom view modifiers and extension of the view struct. Project 3 of Hacking With Swift.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
             .padding()
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct GridStacked<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int,Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View{
+        VStack {
+            Spacer()
+            Text("Views and Modifiers")
+                .titleStyle()
+            Spacer()
+            GridStacked(rows: 4, columns: 4) { row, col in
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+            }
+            Spacer()
+            Spacer()
+        }
     }
 }
 
